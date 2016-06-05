778675bb911f7f1914c1a37dcd0d3662bd015763
manishpeshwani <manishpeshwani@yahoo.com>
2016-06-05 20:02:46 +0530

    Added javadoc

--- src/com/manish/java8/ChainedConsumer.java
+++ src/com/manish/java8/ChainedConsumer.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
+//Example of chained consumer
 public class ChainedConsumer {
 
 	public static void main(String[] args) {
